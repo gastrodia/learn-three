c85742317d278dac528c337ed2282551ac5cd635
gastrodia <3025822868@qq.com>
2023-03-18 17:28:22 +0800

    feat: 🧐 gitHub pages

---
+++ src/webGL/05/script.js
@@ -0,0 +1,27 @@
+import 'uno.css'
+import {createShader} from "@/utils/createShader";
+
+const canvas = document.createElement('canvas')
+const {innerWidth: WIDTH, innerHeight: HEIGHT} = window
+document.body.append(canvas)
+
+canvas.width = WIDTH
+canvas.height = HEIGHT
+
+const gl = canvas.getContext('webgl')
+
+const VERTEX_SHADER_SOURCE = `
+        attribute vec4 onePosition;
+        void main() {
+            gl_Position = onePosition;
+            gl_PointSize = 10.0;
+        }
+    `
+const FRAGMENT_SHADER_SOURCE = `
+        void main() {
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+`
+
+const program = createShader(gl, VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE)
+
