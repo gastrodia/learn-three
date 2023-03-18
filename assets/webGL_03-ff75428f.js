2b745b99135485eed3ae7ce739827854c40ca61f
gastrodia <96605737+gastrodia@users.noreply.github.com>
2023-03-18 09:32:34 +0000

    Deploying to dist from @ gastrodia/learn-three@4dbdd8553579e295a2d5b541cb47981f3539d07b 🚀

---
+++ assets/webGL_03-ff75428f.js
@@ -0,0 +1,11 @@
+import"./__uno-9d578755.js";import{c}from"./createShader-b7bbb4a8.js";const n=document.createElement("canvas"),{innerWidth:s,innerHeight:a}=window;document.body.append(n);n.width=s;n.height=a;const t=n.getContext("webgl"),d=`
+        attribute vec4 onePosition;
+        void main() {
+            gl_Position = onePosition;
+            gl_PointSize = 100.0;
+        }
+    `,g=`
+        void main() {
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+`,m=c(t,d,g),l=t.getAttribLocation(m,"onePosition"),e=100/1e3/2,E=.01;let o=e,i=1;const r=()=>{(o>1-e||o<e)&&(i*=-1),o+=E*i,t.vertexAttrib1f(l,o),t.drawArrays(t.POINTS,0,1),requestAnimationFrame(r)};r();
