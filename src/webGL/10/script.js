45020cbdaeb520e15495723df712d5d8de555fb6
gastrodia <3025822868@qq.com>
2023-03-26 10:41:42 +0800

    feat(webGL): ⛄ shader transform

---
+++ src/webGL/10/script.js
@@ -0,0 +1,58 @@
+import 'uno.css'
+import {createShader} from "@/utils/createShader";
+import {AnimationController} from "@/utils/animationController";
+
+const canvas = document.createElement('canvas')
+const {innerWidth: WIDTH, innerHeight: HEIGHT} = window
+document.body.append(canvas)
+
+canvas.width = WIDTH
+canvas.height = HEIGHT
+canvas.style.backgroundColor = 'powderblue'
+
+const gl = canvas.getContext('webgl')
+
+const VERTEX_SHADER_SOURCE = `
+        attribute vec4 onePosition;
+        attribute float oneTranslate;
+        void main() {
+            gl_Position = vec4(onePosition.x + oneTranslate, onePosition.y, onePosition.z, 1.0);
+        }
+    `
+const FRAGMENT_SHADER_SOURCE = `
+        void main() {
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+`
+
+const program = createShader(gl, VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE)
+
+const onePosition = gl.getAttribLocation(program, 'onePosition')
+const oneTranslate = gl.getAttribLocation(program, 'oneTranslate')
+
+const points = new Float32Array([
+    0.0, 0.1,
+    -0.1, -0.1,
+    0.1, -0.1
+])
+
+const buffer = gl.createBuffer()
+
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+
+gl.bufferData(gl.ARRAY_BUFFER, points, gl.STATIC_DRAW)
+
+const size = 2
+gl.vertexAttribPointer(onePosition, size, gl.FLOAT, false, 0, 0)
+
+gl.enableVertexAttribArray(onePosition)
+
+let step = 0.01
+let direction = 1
+let x = -1
+new AnimationController(() => {
+    if (x > 1 || x < -1) direction *= -1
+    x += step * direction
+    gl.vertexAttrib1f(oneTranslate, x)
+    gl.drawArrays(gl.TRIANGLES, 0, points.length / size)
+}, 60)
---
+++ src/webGL/12/script.js
@@ -0,0 +1,60 @@
+import 'uno.css'
+import {createShader} from "@/utils/createShader";
+import {AnimationController} from "@/utils/animationController";
+
+const canvas = document.createElement('canvas')
+const {innerWidth: WIDTH, innerHeight: HEIGHT} = window
+document.body.append(canvas)
+
+canvas.width = WIDTH
+canvas.height = HEIGHT
+canvas.style.backgroundColor = 'powderblue'
+
+const gl = canvas.getContext('webgl')
+
+const VERTEX_SHADER_SOURCE = `
+        attribute vec4 onePosition;
+        attribute float oneRotate;
+        void main() {
+            gl_Position.x = onePosition.x * cos(oneRotate) - onePosition.y * sin(oneRotate);
+            gl_Position.y = onePosition.x * sin(oneRotate) + onePosition.y * cos(oneRotate);
+            gl_Position.z = onePosition.z;
+            gl_Position.w = onePosition.w;
+        }
+    `
+const FRAGMENT_SHADER_SOURCE = `
+        void main() {
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+`
+
+const program = createShader(gl, VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE)
+
+const onePosition = gl.getAttribLocation(program, 'onePosition')
+const oneRotate = gl.getAttribLocation(program, 'oneRotate')
+
+const points = new Float32Array([
+    0.0, 0.1,
+    -0.1, -0.1,
+    0.1, -0.1
+])
+
+const buffer = gl.createBuffer()
+
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+
+gl.bufferData(gl.ARRAY_BUFFER, points, gl.STATIC_DRAW)
+
+const size = 2
+gl.vertexAttribPointer(onePosition, size, gl.FLOAT, false, 0, 0)
+
+gl.enableVertexAttribArray(onePosition)
+
+let step = 0.02
+let direction = 1
+let x = 0
+new AnimationController(() => {
+    x += step * direction
+    gl.vertexAttrib1f(oneRotate, x)
+    gl.drawArrays(gl.TRIANGLES, 0, points.length / size)
+}, 60)
